aa461ca1f690f1c1c141c5ff5974e0dadeb038b3
Chintu0013 <zbcdabcd1290@gmail.com>
2025-11-05 19:46:03 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -4,7 +4,7 @@ from os import environ
 # API Configuration
 API_ID = int(os.environ.get("API_ID", "25933223"))
 API_HASH = os.environ.get("API_HASH", "6ef5a426d85b7f01562a41e6416791d3")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "AAGnNkx1jYS7c-PdQkwAJ4epnMerAcabIA4")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 CREDIT = os.environ.get("CREDIT", "࿐ཽ༵༒ C H I N T U💛༒࿐ཽ༵")
 # MongoDB Configuration
@@ -13,7 +13,7 @@ DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://sujalbot:sujalbot@c
 MONGO_URL = DATABASE_URL  # For auth system
 
 # Owner and Admin Configuration
-OWNER_ID = int(os.environ.get("OWNER_ID", "7341535618"))
+OWNER_ID = int(os.environ.get("OWNER_ID", ""))
 ADMINS = [8458169280, 8383373235]  # dono admins id list me
 
 
@@ -60,6 +60,7 @@ Please contact the admin @ItsUGBot to get access.</blockquote>""",
 
 <blockquote>Use format: {format}</blockquote>"""
 }
+
 
 
 
